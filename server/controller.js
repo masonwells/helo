69310f404b34d075d7d97701a9dd388597ce6bc8
masonwells <masonwbrockbank@gmail.com>
2018-07-27 16:40:15 -0600

    Right before sessions

--- server/controller.js
+++ server/controller.js
@@ -1,14 +1,10 @@
 module.exports ={
   
   login: (req, res) =>{
-    const db = req.app.get('db') //set up connection with db
-    const {username, password} = req.body //deconstruct from front end or body
-    db.get_user([username])
-    .then(user => {
-      if (res.username === username & res.password === password){
-      res.status(200).send({id, username, profile_pic})
-      }else{send.status(400, 'gtfo')}
-    })
+    const db = req.app.get("db")
+    const {username, password} = req.body
+    db.get_user([username, password])
+    .then(user => res.status(200).send(user))
     .catch(()=>res.status(500).send())
   },
 
@@ -16,11 +12,31 @@ module.exports ={
     const db = req.app.get("db")
     const {username, password} = req.body
     db.create_user([username, password, `profilePicture=${username}`])
-    .then(res=>{
-        const {id, username, profile_pic} = res[0]
-        req.session.userid = username;
-        res.status(200).send(id, username, profile_pic)})
+    .then(user => res.status(200).send(user))
+    .catch(()=>res.status(500).send())
   },
+
+  // login: (req, res) =>{
+  //   const db = req.app.get('db') //set up connection with db
+  //   const {username, password} = req.body //deconstruct from front end or body
+  //   db.get_user([username])
+  //   .then(user => {
+  //     if (res.username === username & res.password === password){
+  //     res.status(200).send({id, username, profile_pic})
+  //     }else{send.status(400, 'gtfo')}
+  //   })
+  //   .catch(()=>res.status(500).send())
+  // },
+
+  // register: (req, res) =>{
+  //   const db = req.app.get("db")
+  //   const {username, password} = req.body
+  //   db.create_user([username, password, `profilePicture=${username}`])
+  //   .then(res=>{
+  //       const {id, username, profile_pic} = res[0]
+  //       req.session.userid = username;
+  //       res.status(200).send(id, username, profile_pic)})
+  // }
 
   getPosts: (req, res) =>{
     const db = req.app.get("db")
